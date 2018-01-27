459d8964863e2792fc790ad763b0fb5b626eb161
SergiCebrianGres <sergicebriangres@gmail.com>
2018-01-27 20:29:09 +0100

    Smooth Camera and Sergi Scene

---
+++ GGJ2018/Assets/scripts/SmoothCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour {
+    public Transform lookAt;
+
+    public bool smooth = true;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset = new Vector3(0, 5, -10);
+	// Use this for initialization
+	void Start () {
+        transform.position = lookAt.position + offset;
+        transform.LookAt(lookAt);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 desired = lookAt.position + offset;
+
+        if (smooth)
+        {
+            transform.position = Vector3.Lerp(transform.position, desired, smoothSpeed);
+        }
+        else
+        {
+            transform.position = desired;
+        }
+	}
+}
