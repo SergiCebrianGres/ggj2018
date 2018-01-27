9ca259db417183ca20818d6b64396d86cbabf5f9
Aliarachan <kaala_llunaa@hotmail.com>
2018-01-27 19:27:54 +0100

    reorder

--- GGJ2018/Assets/scripts/IndependentMovementController.cs
+++ GGJ2018/Assets/scripts/IndependentMovementController.cs
@@ -9,23 +9,30 @@ public class IndependentMovementController : MonoBehaviour {
     private bool shouldMove;
     NavMeshAgent agent;
     GameObject tt;
+    Animator anim;
 
     void Start()
     {
         shouldMove = false;
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     void Update(){
         if (shouldMove)
         {
             agent.SetDestination(target);
+            anim.SetFloat("Forward", agent.velocity.magnitude / (agent.speed*2));
             if (pathComplete())
             {
                 shouldMove = false;
                 GetComponent<IAController>().NotifyEndOfRoad();
+
             }
 
+        } else
+        {
+            anim.SetFloat("Forward", 0f);
         }
     }
 
@@ -53,12 +60,12 @@ public class IndependentMovementController : MonoBehaviour {
     protected bool pathComplete()
     {
         //Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance
-        if (Vector3.Distance(agent.destination, agent.transform.position) <= 1f)
+        if (Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance)
         {
-            //if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
-            //{
+            if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+            {
                 return true;
-            //}
+            }
         }
 
         return false;
