7ea7c23f1b01c80047171c084eb6e73abb75f8e3
Aliarachan <kaala_llunaa@hotmail.com>
2018-01-27 15:30:14 +0100

    happybar

---
+++ GGJ2018/Assets/scripts/HappyBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HappyBar : MonoBehaviour {
+
+    [SerializeField]
+    private Image content;
+
+    [SerializeField]
+    private Color fullColor;
+    [SerializeField]
+    private Color lowColor;
+    private float fillAmount;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fillAmount = content.fillAmount - 0.001f;
+        content.fillAmount = fillAmount;
+        content.color = Color.Lerp(lowColor, fullColor, fillAmount);
+
+	}
+
+
+
+}
