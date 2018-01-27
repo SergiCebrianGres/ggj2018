8fe881ab21734b8b9ac7a555fe5fa75f522eccd3
Aliarachan <kaala_llunaa@hotmail.com>
2018-01-27 22:02:05 +0100

    computer controller & colliders

---
+++ GGJ2018/Assets/scripts/ComputerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerController : MonoBehaviour {
+    private bool connected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool IsConnected()
+    {
+        return connected;
+    }
+
+    public void ChangeConnection(bool connection)
+    {
+        connected = connection;
+    }
+
+}
