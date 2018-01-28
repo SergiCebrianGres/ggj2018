d4474c926456f9a7b7f2d751d812f168ca482ca2
Aliarachan <kaala_llunaa@hotmail.com>
2018-01-28 12:38:21 +0100

    timer!

---
+++ GGJ2018/Assets/scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public Text timerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = ((int)t % 60).ToString("00");
+
+        timerText.text = minutes + ":" + seconds;
+    }
+}
